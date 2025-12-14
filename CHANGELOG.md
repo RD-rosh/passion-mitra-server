ebb0da42c79bcccf46a020dbf0603ba3c0395645
RD-rosh <roshmi1997@gmail.com>
2025-12-14 11:06:22 +0530

    Test GetDIseases endpt with prolog KB

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,3 +10,9 @@ body {
 "wilting_stem"
 ]
 }
+
+Remove create disease and now used prolog KB for diagnoses
+
+http://localhost:3000/prolog/diagnose?symptoms=wilting_stem,yellow_leaves
+
+{"result":["[fusarium_wilt-0.9,fusarium_wilt-0.8,vine_mite-0.8,waterlogging-0.8]"]}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiagnosisModule } from './diagnosis/diagnosis.module';
 import { VarietyModule } from './variety/variety.module';
+import { PrologService } from './prolog/prolog.service';
+import { PrologController } from './prolog/prolog.controller';
 
 @Module({
   imports: [DiagnosisModule, VarietyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PrologController],
+  providers: [AppService, PrologService],
 })
 export class AppModule {}
